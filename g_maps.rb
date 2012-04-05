bea20aa7add3b72a404027d23e1361ecf0a9eb20
Tom Wey <tjmwey@gmail.com>
2012-04-05 22:38:05 +0100

    More generic-ness

--- g_maps.rb
+++ g_maps.rb
@@ -33,6 +33,34 @@ class GMaps < Sinatra::Base
             stations << station_data
         end
 
+        haml :kml, :locals => {
+            :data => stations,
+            :icon => 'http://freezing-winter-3097.heroku.com/cycle-hire-pushpin-icon.gif'
+        }
+    end
+
+    get '/allotments.kml' do
+        content_type 'text/xml'
+
+        allotments_url = URI.parse("http://dl.dropbox.com/u/6313902/gla-allotment-locations.csv")
+
+        tfl_xml = REXML::Document.new(
+            Net::HTTP.get_response(tfl_url).body
+        )
+
+        stations = []
+
+        tfl_xml.elements.each('//stations/station') do |station|
+            station_data = {}
+
+            station_data['name']        = station.get_text('name')
+            station_data['description'] = station.get_text('terminalName')
+            station_data['lat']         = station.get_text('lat')
+            station_data['long']        = station.get_text('long')
+
+            stations << station_data
+        end
+
         haml :tfl_bikes_kml, :locals => { :tfl_bike_data => stations }
     end
 end
